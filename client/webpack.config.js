e79022274971b5b6d9765740f1ebe1e41f7a5088
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2023-11-04 23:25:26 +0530

    run client and server concurrently

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './client/src/index.js',
     output: {
         path: path.resolve(__dirname, './build'),
         filename: 'bundle.js'
@@ -22,7 +22,7 @@ module.exports = {
     },
     plugins: [
        new HtmlWebpackPlugin({
-        template: './src/index.html'
+        template: './client/src/index.html'
        })
     ]
 }
