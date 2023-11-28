45d786bba461762df42f0f3225340afc97e9e8de
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2023-11-28 16:13:48 +0530

    clean up

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {
-    getImages
-} = require('../controllers/index')
-
-router.get('/images', getImages)
-
-module.exports =  router
