a3639dccf205d23c71ac54b0ea7f0432cf07c8eb
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2023-12-07 11:05:02 +0530

    user signup - model, controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,41 @@
+const User = require("../models/usermodel");
+
+const register = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const userData = {
+            username,
+            password,
+        };
+
+        await User.create(userData)
+            .then((task) => {
+                if (!task) {
+                    return res.status(404).json({
+                        success: false,
+                        message: "user not created",
+                    });
+                }
+
+                res.status(201).json({
+                    sucess: true,
+                    message: "user created",
+                });
+            })
+            .catch((error) => {
+                res.status(404).json({
+                    success: false,
+                    error: error.message,
+                });
+            });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: "Internal server error",
+        });
+    }
+};
+
 const loginPage = (req, res) => {
     res.render("pages/login");
 };
@@ -11,12 +49,6 @@ const login = (req, res) => {
 
 const signupPage = (req, res) => {
     res.render("pages/signup");
-};
-
-const register = (req, res) => {
-    res.json({
-        message: "user created ",
-    });
 };
 
 module.exports = {
