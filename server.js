8b4277e3f90e4b433086f39de565e668df4e804c
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2023-12-07 10:15:48 +0530

    mongodb connected

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const routes = require("./routes/uploadRoutes");
 const authRoutes = require("./routes/authRoutes");
+const connectDB = require("./db/db");
 const dotenv = require("dotenv");
+
 dotenv.config();
+connectDB();
 
 const app = express();
+app.use(express.json());
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
-
-app.use(express.json());
 
 app.use("/", routes);
 app.use(
