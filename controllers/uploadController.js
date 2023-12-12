a5715637792323bebe17b74f40421ffe1c4a2337
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2023-12-12 10:58:32 +0530

    connect to s3 and list buckets

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,10 +1,35 @@
+const { S3Client, ListBucketsCommand } = require("@aws-sdk/client-s3");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+let client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+    },
+});
+
 const uploadPage = (req, res) => {
     res.render("pages/upload");
 };
 
-const uploadFiles = (req, res) => {
-    console.log(req.body.name);
-    console.log(req.body.files);
+const uploadFiles = async (req, res) => {
+    const command = new ListBucketsCommand({});
+
+    try {
+        const { Owner, Buckets } = await client.send(command);
+        console.log(
+            `${Owner.DisplayName} owns ${Buckets.length} bucket${
+                Buckets.length === 1 ? "" : "s"
+            }:`
+        );
+        console.log(`${Buckets.map((b) => ` • ${b.Name}`).join("\n")}`);
+    } catch (error) {
+        console.log(error);
+    }
+
     res.json({
         message: "upload successful",
     });
